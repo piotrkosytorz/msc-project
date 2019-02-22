e55b4480e732b7e71eadbaec25dea3c6f6b5587f
piotrkosytorz <piotr.kosytorz@gmail.com>
2019-02-22 14:05:06 +0100

    test resources + data importer + gradle config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import algorithms.lftj.LeapFrogTrieJoinQueryResolver;
-import algorithms.nestedloop.IterativeNestedLoopJoinQueryResolver;
-import algorithms.nestedloop.RecursiveNestedLoopJoinQueryResolver;
+import dataimporters.DataImporter;
 import query.Atom;
 import query.Query;
 import query.Relation;
-import query.Tuple;
 
 import java.util.List;
 import java.util.Map;
@@ -13,84 +11,112 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Relation<String> relR = new Relation<>(2);
-        Relation<String> relS = new Relation<>(2);
-        Relation<String> relT = new Relation<>(2);
+//        Relation<String> relR = new Relation<>(2);
+//        Relation<String> relS = new Relation<>(2);
+//        Relation<String> relT = new Relation<>(2);
+//
+//        relR.addAll(
+//                new Tuple<>("3", "6"),
+//                new Tuple<>("6", "4"),
+//                new Tuple<>("7", "4")
+//        );
+//
+//        relS.addAll(
+//                new Tuple<>("3", "1"),
+//                new Tuple<>("3", "2"),
+//                new Tuple<>("3", "3"),
+//                new Tuple<>("4", "0"),
+//                new Tuple<>("4", "1"),
+//                new Tuple<>("4", "2"),
+//                new Tuple<>("4", "3"),
+//                new Tuple<>("4", "4"),
+//                new Tuple<>("4", "5"),
+//                new Tuple<>("4", "9")
+//        );
+//
+//        relT.addAll(
+//                new Tuple<>("6", "2"),
+//                new Tuple<>("6", "3"),
+//                new Tuple<>("7", "0"),
+//                new Tuple<>("7", "1"),
+//                new Tuple<>("7", "2"),
+//                new Tuple<>("7", "3"),
+//                new Tuple<>("7", "5")
+//        );
+//
+//        Query<String> query = new Query<>(
+//                new Atom<>(relR, "a", "b"),
+//                new Atom<>(relS, "b", "c"),
+//                new Atom<>(relT, "a", "c")
+//        );
 
-        relR.addAll(
-                new Tuple<>("3", "6"),
-                new Tuple<>("6", "4"),
-                new Tuple<>("7", "4")
-        );
+        String localDir = System.getProperty("user.dir");
 
-        relS.addAll(
-                new Tuple<>("3", "1"),
-                new Tuple<>("3", "2"),
-                new Tuple<>("3", "3"),
-                new Tuple<>("4", "0"),
-                new Tuple<>("4", "1"),
-                new Tuple<>("4", "2"),
-                new Tuple<>("4", "3"),
-                new Tuple<>("4", "4"),
-                new Tuple<>("4", "5"),
-                new Tuple<>("4", "9")
-        );
+        String path = localDir + "/src/main/resources/Wiki-Vote.txt";
 
-        relT.addAll(
-                new Tuple<>("6", "2"),
-                new Tuple<>("6", "3"),
-                new Tuple<>("7", "0"),
-                new Tuple<>("7", "1"),
-                new Tuple<>("7", "2"),
-                new Tuple<>("7", "3"),
-                new Tuple<>("7", "5")
-        );
 
-        Query<String> query = new Query<>(
-                new Atom<>(relR, "a", "b"),
-                new Atom<>(relS, "b", "c"),
-                new Atom<>(relT, "a", "c")
-        );
-
-        List<Map<String, String>> res;
         long startTime;
         long endTime;
 
-        try {
-            startTime = System.currentTimeMillis();
-            res = query.resolve(new RecursiveNestedLoopJoinQueryResolver());
-            endTime = System.currentTimeMillis();
-            System.out.println("Recursive nested loop total execution time: \n\t" + (endTime - startTime) + "ms\n");
-            System.out.println("Recursive nested loop result: \n\t" + res);
-        } catch (Exception e) {
-            System.out.println("Recursive nested loop fail!: \n\t" + e.getLocalizedMessage());
-        }
 
-        System.out.println("\n======================================\n");
-
-        try {
-            startTime = System.currentTimeMillis();
-            res = query.resolve(new IterativeNestedLoopJoinQueryResolver());
-            endTime = System.currentTimeMillis();
-            System.out.println("Iterative nested loop total execution time: \n\t" + (endTime - startTime) + "ms\n");
-            System.out.println("Iterative nested loop result: \n\t" + res);
-        } catch (Exception e) {
-            System.out.println("Iterative nested loop fail!: \n\t" + e.getLocalizedMessage());
-        }
-
-        System.out.println("\n======================================\n");
-
-
-        try {
         startTime = System.currentTimeMillis();
-        res = query.resolve(new LeapFrogTrieJoinQueryResolver());
+        Relation<Integer> relA = DataImporter.importFromFile(path);
+        Relation<Integer> relB = DataImporter.importFromFile(path);
+        Relation<Integer> relC = DataImporter.importFromFile(path);
         endTime = System.currentTimeMillis();
 
-        System.out.println("LFTJ total execution time: \n\t" + (endTime - startTime) + "ms\n");
-        System.out.println("LFTJ result: \n\t" + res);
+        System.out.println("Importers execution time: \n\t" + (endTime - startTime) + "ms\n");
+
+        startTime = System.currentTimeMillis();
+        Query<Integer> query = new Query<>(
+                new Atom<>(relA, "a", "b"),
+                new Atom<>(relB, "b", "c"),
+                new Atom<>(relC, "a", "c")
+        );
+        endTime = System.currentTimeMillis();
+        System.out.println("Query builder time: \n\t" + (endTime - startTime) + "ms\n");
+
+        List<Map<String, Integer>> res;
+
+
+
+//        try {
+//            startTime = System.currentTimeMillis();
+//            res = query.resolve(new RecursiveNestedLoopJoinQueryResolver());
+//            endTime = System.currentTimeMillis();
+//            System.out.println("Recursive nested loop total execution time: \n\t" + (endTime - startTime) + "ms\n");
+//            System.out.println("Recursive nested loop result: \n\t" + res);
+//        } catch (Exception e) {
+//            System.out.println("Recursive nested loop fail!: \n\t" + e.getLocalizedMessage());
+//        }
+//
+//        System.out.println("\n======================================\n");
+
+//        try {
+//            startTime = System.currentTimeMillis();
+//            res = query.resolve(new IterativeNestedLoopJoinQueryResolver());
+//            endTime = System.currentTimeMillis();
+//            System.out.println("Iterative nested loop total execution time: \n\t" + (endTime - startTime) + "ms\n");
+//            System.out.println("Iterative nested loop result: \n\t" + res);
+//        } catch (Exception e) {
+//            System.out.println("Iterative nested loop fail!: \n\t" + e.getLocalizedMessage());
+//        }
+//
+//        System.out.println("\n======================================\n");
+//
+
+        try {
+            startTime = System.currentTimeMillis();
+            res = query.resolve(new LeapFrogTrieJoinQueryResolver());
+            endTime = System.currentTimeMillis();
+
+            System.out.println("LFTJ number of results: " + res.size());
+            System.out.println("LFTJ total execution time: \n\t" + (endTime - startTime) + "ms\n");
+//            System.out.println("LFTJ result: \n\t" + res);
         } catch (Exception e) {
             System.out.println("LFTJ fail!: \n\t" + e.getLocalizedMessage());
         }
+
     }
 
 }
